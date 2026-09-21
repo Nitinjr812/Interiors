86d3ed361a6d8f7838c62885f706df5fd833bc75
Nitinjr812 <nitinjr812@gmail.com>
2026-09-21 10:07:13 +0530

    subscription dynamics

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         {/* <Route path="/" element={<LandingPage />} /> */}
         <Route path="/" element={<LandingPage />} />
-        <Route path="/gallery" element={< />} />
+        <Route path="/gallery" element={<GalleryPage/>} />
       </Routes>
     </BrowserRouter>
   )
